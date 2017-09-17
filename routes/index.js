ae845407fba825dae616918b0824ac57583b0ab3
Jae Seok <t-jseok@ean.com>
2017-09-17 15:51:23 +0100

    step3 - method that verifies user github repository

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,13 @@ var request = require('request-promise'); // api request
 
 var adminEmail = 'jaetest94@gmail.com'
 
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: adminEmail,
+    pass: 'jaetest94!'
+  }
+});
 // main page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Indorse - Github verification page' });
@@ -87,130 +94,66 @@ router.get('/verification', function(req, res){
 router.post('/addVerification', function(req, res){
   // assign internal DB variable
   var db = req.db;
-
   // TODO : Get the user name from the user's authentication. (Currently receiving from uesr input)
-  var userName = req.body.username;
+  var userName = 'jaeseokan94'
   var userEmail = req.body.useremail;
   var userHash = 'cb71bc305bfde6f67af6ee1959e67d54'; // get this from database
-  var isVerified = '';
-  var api = GithubRepo(userName);
 
-/*
-  request(api,function(error, response, body){
-    if (!error && response.statusCode == 200) {
-      var info = JSON.parse(body)
-      console.log(info);
-      console.log('Getting a request from ' + api);
-    }else{
-      console.log(error);
-      console.log('response status code : '+response.statusCode);
-    }
 
-  })*/
-// TODO : working in progress
-var github = {
 
-  checkHash: function(repo,hash){
-    return repo.includes(hash);
-  },
-
-  getUserRepos: function(userId,hash){
-    return request({
-      "method":"GET",
-      "uri": 'https://api.github.com/users/'+userId+'/repos?sort=created',
-      "json": true,
-      "headers": {
-        "User-Agent": "node.js"
-      }
-    });
-  }
-
-}
-
-function main(userId,hash){
-  console.log('aaa');
-    return github.getUserRepos(userId,hash).then(checkHash);
-}
-console.log('check');
-
-main(userName,userHash);
-
+  var isHash = new RegExp(userHash,'g'); // find a string that has this hash
+  var isUser = new RegExp(userName,'g');
+  var hasRepo = false;
 
   var collection = db.get('usercollection');
 
-  var mailOptions = {
-    from: adminEmail,
-    to: userEmail,
-    subject: 'Confirm your Github integration',
-    html: '<h1> Are you ' + userName + ' ? </h1>' +
-    '<p> To link your Github repository, please create a repository name with   ' +
-    userHash +
-    '<br> <p> After creating the repository, please click '+'Confirm'+
-    ' to complete verification </p>'
-  };
 
-  collection.insert({
-    "username" : userName,
-    "email" : userEmail,
-    "hash" : userHash,
-    "isVerified" : isVerified
-
-  }, function(err, doc){
-    if(err){
-      res.send("Error : Unable to add the user email / erorr : " + err);     // debug message
-
+  request({
+    "method":"GET",
+    "uri":'https://api.github.com/users/'+userName+'/repos?sort=created',
+    "json": true,
+    "headers":{
+      "User-Agent": "node.js"
     }
-    else{
-      transporter.sendMail(mailOptions, function(error, info){
-          if (error) {
-              console.log(error);
-            } else {
-              console.log('Email sent: ' + info.response);
-  }
-});
-// direct to userlist page
-res.redirect("userlist");
-}
-});
-
-});
-
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: adminEmail,
-    pass: 'jaetest94!'
-  }
-});
-
-// This method checks if hash exist , this method could be used to verify user's social media account using REST API
-// Verification need to be done before proceeding to the verification confirmed page.
-// Input
-// userId - social media ID
-// hash - hash code that user would like to Verify
-// source - source that user would like to verify eg) github, linkedIn etc
-// Output
-// True or False
-function RequestAPI(api) {
-  var body = request('GET',api);
-
-  var body = request(api,function(error, response, body){
-    if (!error && response.statusCode == 200) {
-      var info = JSON.parse(body)
-      console.log(info);
+  }).then(function(data){
+    return data;
+  }).filter(function(data){
+    console.log('this is null ' + data);
+    return data.name.match(isHash);
+  }).then(function(data){
+    // repository not exist
+        if(data[0]==undefined){
+          res.send('Error : Repository ' + userHash + ' does not exist');
+        }
+    var repoOwner = data[0].owner.login;
+    var hasRepo = isUser.test(repoOwner);
+    return hasRepo;
+  }).then(function(hasRepo){
+        // updating database
+        collection.insert({
+          "username" : userName,
+          "email" : userEmail,
+          "hash" : userHash,
+          "isVerified" : hasRepo
+        }, function(err, doc){
+          if(err){
+            res.send("Error : " + err);     // debug message
+          }
+          else{
+            // Added
+            res.send('Successful : Github ID ' + userName + ' is integrated to ' + userEmail );
+          }
+      });
+  }).catch(function(err){
+    console.log('error code : '+err.statusCode);
+    console.log(err);
+    if(err.statusCode==404){
+      res.send('Github username "'+userName + '" does not exist');
+    }else{
+      res.send('Error : Unable to verify Github user : ' + userName);
     }
+  });
+});
 
-  })
-
-}
-// API need to be returned before calling RequestAPI function
-// Input
-// userId - github username
-// Output
-// API
-function GithubRepo(userId){
-    // sort value = created day, since hash named repository is likely to be created most recently, it is more effecient in checking if hash value exist from the result JSON
-    return results = 'https://api.github.com/users/'+userId+'/repos?sort=created';
-}
 
 module.exports = router;
